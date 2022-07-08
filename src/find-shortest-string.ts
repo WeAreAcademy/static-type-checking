ae0a8f7dbafcd9451c5e0fce88591082e6cae0cc
Salman H <Salman0112358@outlook.com>
2022-07-08 18:17:41 +0100

    bug fix and formatting checks

--- src/find-shortest-string.ts
+++ src/find-shortest-string.ts
@@ -1,4 +1,3 @@
-
 /**
  * Find the shortest string in an array.
  * In the case of a tie, returns the earlier string in the array.
@@ -6,14 +5,14 @@
  * @param stringArr array of strings
  * @returns the shortest string
  */
- function findShortestString(stringArr: string[]): string {
-    let shortestString = stringArr[0];
-    for (const str of stringArr) {
-      if (str.length < shortestString.length) {
-        shortestString = str;
-      }
+function findShortestString(stringArr: string[]): string {
+  let shortestString = stringArr[0];
+  for (const str of stringArr) {
+    if (str.length < shortestString.length) {
+      shortestString = str;
     }
-    return shortestString.length;
   }
-  
-  export default findShortestString;
+  return shortestString.length;
+}
+
+export default findShortestString;
